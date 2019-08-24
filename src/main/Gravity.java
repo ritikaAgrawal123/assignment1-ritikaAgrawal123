ce2a4a199baf7520be6bc564f47ae77c9fd65c6c
ritikaAgrawal123 <ritika.agrawal_cs18@gla.ac.in>
2019-08-24 09:25:25 -0700

    add variable; time double, distance double, speed double,

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -14,8 +14,10 @@ public class Gravity {
 	public static void main(String[] args) {
 		double gravityConst = 9.8;
 		Scanner scanner = new Scanner(System.in);
-
-
+		double time = scanner.nextDouble();
+		double distance = (gravityConst * time * time) / 2;
+		double speed = gravityConst * time;
+		scanner.close();
 
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
