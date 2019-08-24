2ca907f457f0bc91fe12ed925d5e5ea912ca916d
ritikaAgrawal123 <ritika.agrawal_cs18@gla.ac.in>
2019-08-24 09:20:34 -0700

    add variable; gravityConst double

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -10,11 +10,9 @@ package main;
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
-		/* TODO 2:
-		 *   a. Declare all your necessary variables inside main method.
-		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
-		 *   c. Calculate the result and print it to the screen.
-		 * */
+		double gravityConst = 9.8;
+
+
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
